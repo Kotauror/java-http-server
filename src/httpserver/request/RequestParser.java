525be397f98464d8c1667d4aa0852a73f680d98f
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-01 15:55:34 +0100

    Test & Feature: parser gets the path from request string.

--- src/httpserver/request/RequestParser.java
+++ src/httpserver/request/RequestParser.java
@@ -7,7 +7,16 @@ public class RequestParser {
     public RequestParser() {}
 
     public Method getMethod(String inputString) {
-        String parts[] = inputString.split(" ", 2);
-        return Method.valueOf(parts[0]);
+        String elementsOfFirstLine[] = inputString.split(" ", 2);
+        return Method.valueOf(elementsOfFirstLine[0]);
+    }
+
+    public String getPath(String inputString) {
+        String elementsOfFirstLine[] = inputString.split(" ", 2);
+        return stripOfNewLine(elementsOfFirstLine[1]);
+    }
+
+    private String stripOfNewLine(String string) {
+        return string.replaceAll("[\n\r]", "");
     }
 }
--- test/RequestParserTests.java
+++ test/RequestParserTests.java
@@ -17,10 +17,10 @@ public class RequestParserTests {
     @Before
     public void setup() {
         requestParser = new RequestParser();
-        input = "GET http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1" +
-                "Host: 0.0.0.0:5000" +
-                "Content-Length: 24" +
-                "method body\r\nmethod body";
+        input = "GET http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1\n" +
+                "Host: 0.0.0.0:5000'\n" +
+                "Content-Length: 24\n" +
+                "method body\r\nmethod body\n";
         headers = new HashMap<String, String>() {{
             put("Host", "0.0.0.0:5000");
             put("Content-Length", "24");
@@ -32,5 +32,11 @@ public class RequestParserTests {
         assertEquals(Method.GET, requestParser.getMethod("GET http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1"));
         assertEquals(Method.OPTIONS, requestParser.getMethod("OPTIONS http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1"));
         assertEquals(Method.POST, requestParser.getMethod("POST http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1"));
+    }
+
+    @Test
+    public void getPathReturnsRightPath() {
+        String path = "http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1";
+        assertEquals(path, requestParser.getPath("GET http://developer.mozilla.org/en-US/docs/Web/HTTP/Messages HTTP/1.1\n"));
     }
 }
