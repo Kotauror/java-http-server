97b31dec0a85cc2a591fdeeb5b5b9e379a5b95dc
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-01 14:40:58 +0100

    Feature: create request class with getters.

--- src/httpserver/request/Request.java
+++ src/httpserver/request/Request.java
@@ -1,4 +1,35 @@
 package httpserver.request;
 
+import java.util.HashMap;
+
 public class Request {
+
+    private final String method;
+    private final String path;
+    private final HashMap<String, String> headers;
+    private final String body;
+
+    public Request(String method, String path, HashMap<String, String> headers, String body) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public String getMethod() {
+        return this.method;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public HashMap getHeaders() {
+        return this.headers;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
 }
