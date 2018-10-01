42844c91e076fcb029419ec44d4f99dedf065f6a
Justyna Zygmunt <justynazygmunt@justynas-air.home>
2018-10-01 19:23:13 +0100

    Update: remove unnecessary lines in App and AppTests.

--- src/httpserver/App.java
+++ src/httpserver/App.java
@@ -1,3 +1,5 @@
+package httpserver;
+
 import java.io.IOException;
 
 public class App {
@@ -5,9 +7,5 @@ public class App {
     public static void main(String[] args) throws IOException {
         int portNumber = Integer.parseInt(args[1]);
         String fileDirectory = args[3];
-    }
-
-    public int multiply(int a, int b) {
-        return a * b;
     }
 }
--- test/AppTests.java
+++ test/AppTests.java
@@ -3,11 +3,4 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTests {
-
-    @Test
-    public void multiplication() {
-        App tester = new App();
-
-        Assert.assertEquals(0, tester.multiply(10, 0));
-    }
 }
