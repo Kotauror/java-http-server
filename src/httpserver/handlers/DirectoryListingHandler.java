9505ef81a003569ea07e24c5c53cfc3fae63d167
Justyna Zygmunt <justynazygmunt@Justynas-MacBook-Air.local>
2018-10-05 15:39:44 +0100

    Tests & Feature: directoryListingHandler returns links.

--- src/httpserver/handlers/DirectoryListingHandler.java
+++ src/httpserver/handlers/DirectoryListingHandler.java
@@ -3,6 +3,7 @@ package httpserver.handlers;
 import httpserver.request.Request;
 import httpserver.response.Response;
 import httpserver.response.ResponseStatus;
+import httpserver.utilities.FileType;
 import httpserver.utilities.Method;
 
 import java.io.File;
@@ -21,15 +22,22 @@ public class DirectoryListingHandler extends Handler {
     public Response getResponse(Request request) {
         File[] files = new File(this.rootPath).listFiles();
         StringBuilder body = new StringBuilder();
+        body.append("<html><head></head><body>");
         for (File file : files) {
-            body.append(file.getName());
+            String link = this.generateLink(file);
+            body.append(link);
         }
+        body.append("</body></html>");
         byte [] bodyContent = body.toString().getBytes();
-        return new Response(ResponseStatus.OK, bodyContent, "text/plain");
+        return new Response(ResponseStatus.OK, bodyContent, FileType.HTML.value());
     }
 
     @Override
     public boolean coversPathFromRequest(Request request) {
         return request.getPath().equals("/");
+    }
+
+    private String generateLink(File file) {
+        return "<a href='/" + file.getName() + "'>" + file.getName() + "</a><br>";
     }
 }
