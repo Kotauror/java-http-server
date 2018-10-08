4770e90cc8363653ec4da7453967a60580f01cb3
Justyna Zygmunt <justynazygmunt@Justynas-MacBook-Air.local>
2018-10-08 15:14:44 +0200

    Refactor: deleteHandlerTests.

--- test/handlersTests/deleteHandlerTests.java
+++ test/handlersTests/deleteHandlerTests.java
@@ -9,43 +9,39 @@ import httpserver.utilities.Method;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class deleteHandlerTests {
 
     private String rootPath;
-    private DeleteHandler deleteHandler;
     private LinkedHashMap<String, String> headers;
     private String httpVersion;
-    private String body;
-    private PutHandler putHandler;
+    private String path;
 
     @Before
-    public void setup() {
+    public void setupAndAddFile() {
         rootPath = "src/httpserver/utilities/sampleTestFiles";
-        deleteHandler = new DeleteHandler(rootPath);
-        putHandler = new PutHandler(rootPath);
         httpVersion = "HTTP/1.1";
         headers = new LinkedHashMap<>();
-        body = "example body";
+        path = "/filetesting";
+
+        PutHandler putHandler = new PutHandler(rootPath);
+        Request request = new Request(Method.GET, path, httpVersion, headers, "Some content");
+        putHandler.processRequest(request);
     }
 
     @Test
     public void deletesFile() {
-        String path = "/filetesting";
-        Request request = new Request(Method.GET, path, httpVersion, headers, "Some content");
-        putHandler.processRequest(request);
-        Request request2 = new Request(Method.DELETE, path, httpVersion, headers, "Some content");
-        Response response = deleteHandler.processRequest(request2);
+        Request request = new Request(Method.DELETE, path, httpVersion, headers, "Some content");
+        DeleteHandler deleteHandler = new DeleteHandler(rootPath);
+        Response response = deleteHandler.processRequest(request);
 
-        assertFalse(Files.exists(Paths.get(rootPath + request2.getPath())));
+        assertFalse(Files.exists(Paths.get(rootPath + request.getPath())));
         assertEquals(ResponseStatus.OK, response.getStatus());
     }
 }
