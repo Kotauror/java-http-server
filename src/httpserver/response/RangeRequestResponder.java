5cd9bc7c384b575ce2772fb9c6f68ba992b7beff
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-09 14:30:17 +0100

    Test & Feature: RangeRequestResponder knows the range of req.

--- src/httpserver/response/RangeRequestResponder.java
+++ src/httpserver/response/RangeRequestResponder.java
@@ -2,9 +2,26 @@ package httpserver.response;
 
 import httpserver.request.Request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RangeRequestResponder {
 
     public Response getRangeResponse(Request request) {
         return new Response(ResponseStatus.RANGE_REQUEST);
+    }
+
+    public Map<String, Integer> getRangeLimits(Request request) {
+        HashMap startEndMap = new HashMap<String, Integer>();
+        String rangeString = request.getHeaders().get("Range").toString();
+        String[] partsOfRangeString = this.getPartsOfRangeString(rangeString);
+        startEndMap.put("start", Integer.parseInt(partsOfRangeString[0]));
+        startEndMap.put("end", Integer.parseInt(partsOfRangeString[1]));
+        return startEndMap;
+    }
+
+    private String[] getPartsOfRangeString(String rangeString) {
+        String numbers = rangeString.substring(6, rangeString.length());
+        return numbers.split("-");
     }
 }
